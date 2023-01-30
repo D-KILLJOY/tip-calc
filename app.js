9562385e793828dea2eeaa8693a3d746d2525bde
Uchenna Didia <didiauche@gmail.com>
2023-01-30 02:56:34 +0100

    complete

---
+++ app.js
@@ -0,0 +1,12 @@
+const money = document.querySelector(".bill");
+const peopleN = document.querySelector(".people");
+const cust = document.querySelector(".cust");
+
+console.log(money.value);
+
+console.log(cust);
+console.log(cust.value);
+
+console.log(peopleN);
+
+console.log(peopleN.value);
